cc1d30a0db9df30cc5588edecd6ba200191e90d6
Developer <dev@example.com>
2026-04-09 13:30:46 +0200

    Streaming made for the revenue

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,10 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div
+          className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12"
+          suppressHydrationWarning
+        >
           <Image
             src="/hero-desktop.png"
             width={1000}
